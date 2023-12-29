20806a95e8a859cc116df5fe3c4c788040b70734
AJ Gilliland <manuelgillilandiv@gmail.com>
2023-12-28 19:40:46 -0600

    Added templates, Linked templates, configured urls views & names, and started work on the base.html

--- app/views.py
+++ app/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render, redirect
 from .models import *
 from .forms import *
+from django.shortcuts import render, redirect
+from django.http.request import HttpRequest
+from django.http.response import HttpResponse
 
-# Create your views here.
+def homePage(request:HttpRequest)->render:
+    context = {}
+    return render(request, "home.html", context)
+
+def communityPage(request:HttpRequest)->render:
+    context = {}
+    return render(request, "community.html", context)
+
+def documentationPage(request:HttpRequest)->render:
+    context = {}
+    return render(request, "documentation.html", context)
+
+def managementPage(request:HttpRequest)->render:
+    context = {}
+    return render(request, "management.html", context)
+
+def procurementPage(request:HttpRequest)->render:
+    context = {}
+    return render(request, "procurement.html", context)
