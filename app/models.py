9e7998986d4244b41569b3ed8192b7c80375a30e
AJ Gilliland <manuelgillilandiv@gmail.com>
2024-01-10 11:20:22 -0600

    made some methods

--- app/models.py
+++ app/models.py
@@ -8,19 +8,43 @@ class Account(models.Model):
 
 class Message(models.Model):
     account = models.ForeignKey(Account, on_delete=models.CASCADE)
-    content = models.CharField(max_length=200)
+    content = models.CharField(max_length=255)
 
 class BugReport(models.Model):
     account = models.OneToOneField(Account, on_delete=models.CASCADE)
+    prompt = models.CharField(max_length=255, null=True)
     messages = models.ForeignKey(Message, related_name='bug_reports_messages', null=True, on_delete=models.SET_NULL)
     most_correct = models.OneToOneField(Message, related_name='bug_report_most_correct', null=True, on_delete=models.SET_NULL)
+    def __str__(self):
+        return self.content
 
 class Notification(models.Model):
     account = models.ForeignKey(Account, on_delete=models.CASCADE)
-    content = models.CharField(max_length=200)
-
+    content = models.CharField(max_length=255)
+    def __str__(self):
+        return self.content
+    
 class Upvote(models.Model):
     accounts = models.ManyToManyField(Account)
     message = models.OneToOneField(Message, on_delete=models.CASCADE)
     date_created = models.DateField(auto_now=True)
+    def __str__(self):
+        return self.message
+
+#### methods ####
+    
+#account   
+def makeAccount(user):
+    account = Account(user=user)     
+    account.save()
+    return account
+
+def getAccountFor(user):
+    account = Account.objects.get(user=user)
+    return account     
+
+#notifications
+def getNotificationsFor(account):
+    notifications = Notification.objects.filter(account=account)
+    return notifications
 
