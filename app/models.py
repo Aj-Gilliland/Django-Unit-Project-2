8816ed462d690795e6a4d4a57bcd34705290e5f7
AJ Gilliland <manuelgillilandiv@gmail.com>
2024-01-10 11:27:35 -0600

    ran mirations

--- app/models.py
+++ app/models.py
@@ -4,10 +4,14 @@ from django.contrib.auth.models import User
 class Account(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.SET_NULL)
     profile_picture = models.ImageField(null=True, blank=True)
+    def __str__(self):
+        return self.user
 
 class Message(models.Model):
     account = models.ForeignKey(Account, on_delete=models.CASCADE)
     content = models.CharField(max_length=255)
+    def __str__(self):
+        return self.content
 
 class BugReport(models.Model):
     account = models.OneToOneField(Account, on_delete=models.CASCADE)
@@ -15,7 +19,7 @@ class BugReport(models.Model):
     messages = models.ForeignKey(Message, related_name='bug_reports_messages', null=True, on_delete=models.SET_NULL)
     most_correct = models.OneToOneField(Message, related_name='bug_report_most_correct', null=True, on_delete=models.SET_NULL)
     def __str__(self):
-        return self.content
+        return self.prompt
 
 class Notification(models.Model):
     account = models.ForeignKey(Account, on_delete=models.CASCADE)
