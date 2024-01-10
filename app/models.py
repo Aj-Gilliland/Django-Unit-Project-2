8f6b4468ecf7fe696f51baebea33fb0062c6c183
AJ Gilliland <manuelgillilandiv@gmail.com>
2024-01-10 10:49:38 -0600

    added comment button and form / styling

--- app/models.py
+++ app/models.py
@@ -3,27 +3,24 @@ from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
 
 class Account(models.Model):
-    user = models.OneToOneField(User)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_picture = models.ImageField(null=True, blank=True)
 
 class Message(models.Model):
     account = models.ForeignKey(Account, on_delete=models.CASCADE)
-    content = models.CharField(max=200)
-    upvote = models.OneToOneField(Upvote)
+    content = models.CharField(max_length=200)
 
 class BugReport(models.Model):
     account = models.OneToOneField(Account, on_delete=models.CASCADE)
-    messages = models.ForeignKey(Message, null=True, on_delete=models.SET_NULL)
-    most_correct = models.OneToOneField(Message, null=True, on_delete=models.SET_NULL)
+    messages = models.ForeignKey(Message, related_name='bug_reports_messages', null=True, on_delete=models.SET_NULL)
+    most_correct = models.OneToOneField(Message, related_name='bug_report_most_correct', null=True, on_delete=models.SET_NULL)
 
 class Notification(models.Model):
     account = models.ForeignKey(Account, on_delete=models.CASCADE)
-    content = models.CharField(max=200)
+    content = models.CharField(max_length=200)
 
 class Upvote(models.Model):
-    accounts = models.ManyToManyField(Account, on_delete=models.CASCADE)
+    accounts = models.ManyToManyField(Account)
     message = models.OneToOneField(Message, on_delete=models.CASCADE)
     date_created = models.DateField(auto_now=True)
 
-# account -> default user field 
-# 
