892d7e44cc9012ad274221f88c181464d85dfc6e
AJ Gilliland <manuelgillilandiv@gmail.com>
2024-01-11 15:12:05 -0600

    commented out ur stuff cuz it broken

--- app/filters.py
+++ app/filters.py
@@ -1,14 +1,14 @@
-import django_filters
-from django_filters import DateFilter, CharFilter
+# import django_filters
+# from django_filters import DateFilter, CharFilter
 
-from .models import *
+# from .models import *
 		
-class BugReportFilter(django_filters.FilterSet):
-	start_date = DateFilter(field_name="date_created", lookup_expr='gte')
-	end_date = DateFilter(field_name="date_created", lookup_expr='lte')
-	prompt = CharFilter(field_name='prompt', lookup_expr='icontains')
+# class BugReportFilter(django_filters.FilterSet):
+# 	start_date = DateFilter(field_name="date_created", lookup_expr='gte')
+# 	end_date = DateFilter(field_name="date_created", lookup_expr='lte')
+# 	prompt = CharFilter(field_name='prompt', lookup_expr='icontains')
 
-	class Meta:
-		model = BugReport
-		fields = '__all__'
-		exclude = ['account', 'messages', 'most_correct' 'date_created']
+# 	class Meta:
+# 		model = BugReport
+# 		fields = '__all__'
+# 		exclude = ['account', 'messages', 'most_correct' 'date_created']
--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "app",
-    "django_filters",
+    # "django_filters",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
