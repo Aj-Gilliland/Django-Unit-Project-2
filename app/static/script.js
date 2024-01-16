6bc0e9421520bfb05e30d2febd9c4178ee56903f
AJ Gilliland <manuelgillilandiv@gmail.com>
2024-01-16 13:18:01 -0600

    cleaned up other misc styles

--- app/static/script.js
+++ app/static/script.js
@@ -5,7 +5,6 @@ function toggleIcon(button) {
 $(document).ready(function () {
     $('#id_picture').change(function () {
         console.log('File input changed');
-        // Trigger the click event of the hidden submit button
         $('#hiddenSubmitButton').click();
     });
 });
